fe78ee2eb3ed7ef98712737b6dd662cad0db5786
Lin, Ying-Chin <mirror770109@gmail.com>
2021-05-04 16:22:06 +0800

    Refactor: Change Function Declaration (147)

--- chapter1/index.js
+++ chapter1/index.js
@@ -8,7 +8,7 @@ function statement(invoice, plays) {
                           { style: "currency", currency: "USD", 
                             minimumFractionDigits: 2}).format;
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf, playFor(perf));
+        let thisAmount = amountFor(perf);
 
         // 加入 volume credit
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -24,9 +24,9 @@ function statement(invoice, plays) {
     return result;
 }
 
-function amountFor(aPerformance, play) {
+function amountFor(aPerformance) {
     let result = 0;
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
         case "tragedy":
             result = 40000;
             if (aPerformance.audience > 30) {
@@ -41,7 +41,7 @@ function amountFor(aPerformance, play) {
             result += 300 * aPerformance.audience;
             break;
         default:
-            throw new Error(`unknown type: ${play.type}`);
+            throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
     return result;
 }
