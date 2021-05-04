e4afe9b459e1fd77a22a3704d9fce20d6d3ba482
Lin, Ying-Chin <mirror770109@gmail.com>
2021-05-04 16:59:48 +0800

    Change variable name

--- chapter1/index.js
+++ chapter1/index.js
@@ -50,19 +50,19 @@ function usd(aNumber) {
 }
 
 function totalVolumeCredits(invoice) {
-    let volumeCredits = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
+        result += volumeCreditsFor(perf);
     }
-    return volumeCredits;
+    return result;
 }
 
 function totalAmount(invoice) {
-    let totalAmount = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-        totalAmount += amountFor(perf);
+        result += amountFor(perf);
     }
-    return totalAmount;
+    return result;
 }
 
 const fs = require("fs");
