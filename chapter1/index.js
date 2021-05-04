65fddadfa6e09450a15bb90f8a7a59cd5d74cd28
Lin, Ying-Chin <mirror770109@gmail.com>
2021-05-04 17:43:48 +0800

    Refactor: Split Phase (181) and Extract Function (126)

--- chapter1/index.js
+++ chapter1/index.js
@@ -1,6 +1,10 @@
 'use strict';
 
 function statement(invoice, plays) {
+    return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
