d241a40ac4559c89f026161b8573045bce51bdd9
Lin, Ying-Chin <mirror770109@gmail.com>
2021-05-05 11:44:40 +0800

    Refactor: Change Function Declaration (147)

--- chapter1/createStatementData.js
+++ chapter1/createStatementData.js
@@ -1,8 +1,9 @@
 'use strict';
 
 class PerformanceCalculator {
-    constructor(aPerformance) {
+    constructor(aPerformance, aPlay) {
         this.performances = aPerformance;
+        this.play = aPlay;
     }
 }
 
@@ -15,9 +16,9 @@ module.exports = function createStatementData(invoice, plays) {
     return result;
 
     function enrichPerformance(aPerformance) {
-        const calculator = new PerformanceCalculator(aPerformance);
+        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
+        result.play = calculator.play;
         result.amount = amountFor(result);
         result.volumeCredits = volumeCreditsFor(result);
         return result;
